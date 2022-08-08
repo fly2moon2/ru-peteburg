eafd71db028f3abce553861f485e1b6eaa351a10
fly2moon2 <fly2moon2@yahoo.com>
2022-08-08 21:17:08 +0800

    ru

--- game/src/main.rs
+++ game/src/main.rs
@@ -10,12 +10,14 @@ pub struct Game {
     pub quit: bool,
     pub player_state: PlayerState,
     pub texture: Texture2D,
+    pub game_speed:f32,
 }
 
 impl Game {
     pub async fn new() -> Self {
         let texture = load_texture("assets/plane.png").await.unwrap();
-
+        //
+       
         Self {
             player_state: PlayerState {
                 position: Vec2::new(100f32, 100f32),
@@ -23,6 +25,7 @@ impl Game {
             },
             texture,
             quit: false,
+            game_speed: 0.6f32,
         }
     }
 
@@ -39,9 +42,18 @@ impl Game {
             self.player_state.rotation -= ROT_SPEED;
         }
 
-        const SPEED: f32 = 0.6;
+        if is_key_down(KeyCode::Up) {
+            self.game_speed = self.game_speed + 0.6;
+        }
+        if is_key_down(KeyCode::Down) {
+            self.game_speed = self.game_speed - 0.2;
+        }
 
-        self.player_state.position += vec2_from_angle(self.player_state.rotation) * SPEED;
+        const SPEED: f32 = 0.6;
+        
+
+        //self.player_state.position += vec2_from_angle(self.player_state.rotation) * SPEED;
+        self.player_state.position += vec2_from_angle(self.player_state.rotation) * self.game_speed;
 
         if self.player_state.position.x > screen_width() {
             self.player_state.position.x = -self.texture.width();
@@ -70,7 +82,7 @@ impl Game {
             },
         );
 
-        draw_box(Vec2::new(400f32, 200f32), Vec2::new(50f32, 20f32));
+        draw_box(Vec2::new(250f32, 200f32), Vec2::new(15f32, 12f32));
     }
 }
 
@@ -89,8 +101,41 @@ fn draw_box(pos: Vec2, size: Vec2) {
 #[macroquad::main("game")]
 async fn main() {
     let mut game = Game::new().await;
+    //
+    let rust_logo = load_texture("assets/plane.png").await.unwrap();
 
     loop {
+        clear_background(LIGHTGRAY);
+
+        // Going 3d!
+
+        set_camera(&Camera3D {
+            position: vec3(-20., 15., 0.),
+            up: vec3(0., 1., 0.),
+            target: vec3(0., 0., 0.),
+            ..Default::default()
+        });
+
+        draw_grid(20, 1., BLACK, GRAY);
+
+        draw_cube_wires(vec3(0., 1., -6.), vec3(2., 2., 2.), DARKGREEN);
+        draw_cube_wires(vec3(0., 1., 6.), vec3(2., 2., 2.), DARKBLUE);
+        draw_cube_wires(vec3(2., 1., 2.), vec3(2., 2., 2.), YELLOW);
+
+
+        draw_cube(vec3(-5., 1., -2.), vec3(2., 2., 2.), rust_logo, WHITE);
+
+        draw_cube(vec3(2., 0., -2.), vec3(0.4, 0.4, 0.4), None, BLACK);
+
+        draw_sphere(vec3(-8., 0., 0.), 1., None, BLUE);
+
+        draw_sphere(vec3(10., 10., 10.), 1., rust_logo, BLACK);
+
+        // Back to screen space, render some text
+
+        set_default_camera();
+        draw_text("WELCOME TO 3D WORLD", 10.0, 20.0, 30.0, BLACK);
+        // 3d example ends
         game.update();
         game.draw();
         if game.quit {
