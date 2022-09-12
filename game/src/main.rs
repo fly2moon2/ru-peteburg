fe9fbf1ce33ee9c500ca6402e4e57cbb1150de07
fly2moon2 <fly2moon2@yahoo.com>
2022-09-12 18:23:54 +0800

    font

--- game/src/main.rs
+++ game/src/main.rs
@@ -1,5 +1,37 @@
 use macroquad::prelude::*;
 
+//use macroquad_font_renderer::Fonts;
+//use macroquad_tiled as tiled;
+/* use macroquad::{
+
+    experimental::{
+        collections::storage,
+        coroutines::start_coroutine,
+
+    },
+    ui,
+}; */
+
+
+//const NOTO_SANS: &[u8] = include_bytes!("../assets/fonts/NotoSans-Regular.ttf");
+
+use std::mem::size_of;
+
+static B:[u8;4]=[34,56,123,23];
+static C:[u8;7]=[3,33,54,231,34,45,98];
+
+
+pub fn init() {
+    let a:usize=42;
+    let b:&[u8;4]=&B;
+    let c:Box<[u8]>=Box::new(C);
+
+    println!("a is: {}",a);
+    println!("location: {:p}",&a);
+
+
+}
+
 #[derive(Default)]
 pub struct PlayerState {
     pub position: Vec2,
@@ -10,24 +42,33 @@ pub struct Game {
     pub quit: bool,
     pub player_state: PlayerState,
     pub texture: Texture2D,
+    pub texture2: Texture2D,
     pub game_speed:f32,
+    pub opacity:f32
 }
 
 impl Game {
     pub async fn new() -> Self {
         let texture = load_texture("assets/plane.png").await.unwrap();
+        let texture2 = load_texture("assets/starship1.png").await.unwrap();
         //
        
+
+
         Self {
             player_state: PlayerState {
                 position: Vec2::new(100f32, 100f32),
                 rotation: 0f32,
             },
             texture,
+            texture2,
             quit: false,
             game_speed: 0.6f32,
+            opacity: 1.0f32,
         }
     }
+
+
 
     pub fn update(&mut self) {
         if is_key_down(KeyCode::Escape) {
@@ -47,6 +88,14 @@ impl Game {
         }
         if is_key_down(KeyCode::Down) {
             self.game_speed = self.game_speed - 0.2;
+        }
+
+        // add/minus opacity
+        if is_key_down(KeyCode::A) && (self.opacity < 1.0) {
+            self.opacity = self.opacity + 0.02;
+        }
+        if is_key_down(KeyCode::D) && (self.opacity > 0.0) {
+            self.opacity = self.opacity - 0.02;
         }
 
         const SPEED: f32 = 0.6;
@@ -71,18 +120,38 @@ impl Game {
     pub fn draw(&self) {
         //clear_background(color_u8!(255, 255, 255, 255));
 
+        // texture
         draw_texture_ex(
             self.texture,
             self.player_state.position.x,
             self.player_state.position.y,
-            WHITE,
+            //WHITE,
+            Color::new(1.00,1.00,1.00,self.opacity),
             DrawTextureParams {
                 rotation: self.player_state.rotation,
                 ..Default::default()
             },
         );
 
-        draw_box(Vec2::new(250f32, 200f32), Vec2::new(15f32, 12f32));
+        //texture2
+        draw_texture_ex(
+            self.texture2,
+            self.player_state.position.x+100f32,
+            self.player_state.position.y+100f32,
+            //WHITE,
+            Color::new(1.00,1.00,1.00,self.opacity),
+            DrawTextureParams {
+                rotation: self.player_state.rotation,
+                ..Default::default()
+            },
+        );
+
+        draw_box(Vec2::new(self.player_state.position.x + 100f32, self.player_state.position.y + 50f32), Vec2::new(15f32, 12f32));
+    
+
+        draw_text_ex("st. petersburg", self.player_state.position.x + 150f32, 120.0, TextParams::default());
+
+        
     }
 }
 
@@ -98,15 +167,111 @@ fn draw_box(pos: Vec2, size: Vec2) {
     draw_rectangle(upper_left.x, upper_left.y, dimension.x, dimension.y, BLACK);
 }
 
+//https://github.com/heroiclabs/fishgame-macroquad/blob/master/src/main.rs
+/* struct Resources {
+
+    tiled_map: tiled::Map,
+    stone:Texture2D,
+
+}
+ */
+/* impl Resources {
+    async fn new() -> Result<Resources, macroquad::prelude::FileError> {
+        let tileset = load_texture("assets/tileset.png").await?;
+        tileset.set_filter(FilterMode::Nearest);
+
+        let stone = load_texture("assets/stone.png").await?;
+        stone.set_filter(FilterMode::Nearest);
+
+    let tiled_map_json = load_string("assets/map.json").await.unwrap();
+    let tiled_map = tiled::load_map(
+        &tiled_map_json,
+        &[("tileset.png", tileset), ("stone.png", stone)],
+        &[],
+    )
+    .unwrap();
+
+    Ok(Resources {
+
+        tiled_map,
+        stone,
+
+    })
+    }
+} */
+
+/* pub fn tiled(){
+
+    let resources_loading = start_coroutine(async move {
+        let resources = Resources::new().await.unwrap();
+    });
+    while resources_loading.is_done() == false {
+    }
+    let resources = storage::get::<Resources>();
+    for object in &resources.tiled_map.layers["stone"].objects {
+        scene::add_node(Decoration::new(
+            vec2(object.world_x, object.world_y),
+            object.gid.unwrap(),
+        ));
+    }
+    drop(resources);
+
+} */
+
 #[macroquad::main("game")]
 async fn main() {
     let mut game = Game::new().await;
     //
     let rust_logo = load_texture("assets/plane.png").await.unwrap();
 
+    //
+    //init()
+    init();
+    //tiled();
+
+    //static mut FONT_SIZE: f32 = 32.0;
+
+    let font = load_ttf_font("./assets/fonts/RobotoCondensed-LightItalic.ttf").await.unwrap();
+    // vertical fonts not supported
+    //let font = load_ttf_font("./assets/fonts/NotoSansHK-Regular.otf").await.unwrap();
     loop {
         clear_background(LIGHTGRAY);
+        
+        draw_text_ex("Custom font size:", 120.0, 120.0, TextParams::default());
+        let mut y = 20.0;
 
+        //https://pullanswer.com/questions/font-sizes-different-between-wasm-native
+
+/*         let textparams = TextParams {
+            font,
+            font_size: unsafe { FONT_SIZE as u16 },
+            color: BLACK,
+            ..Default::default()
+        }; */
+
+        for font_size in (30..100).step_by(20) {
+            let text = "abcdef";
+            let params = TextParams {
+                font,
+                font_size,
+                ..Default::default()
+            };
+
+            y += font_size as f32;
+            draw_text_ex(text, 20.0, y, params);
+        }
+
+        draw_text_ex("Dynamic font scale:", 20.0, 400.0, TextParams::default());
+        draw_text_ex(
+            "abcd",
+            20.0,
+            450.0,
+            TextParams {
+                font_size: 50,
+                font_scale: get_time().sin() as f32 / 2.0 + 1.0,
+                ..Default::default()
+            },
+        );
 /*         set_camera(&Camera2D {
             zoom: vec2(1., screen_width() / screen_height()),
             ..Default::default()
