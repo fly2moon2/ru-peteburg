8212a2f89e02fe937e0f2b1b892aac4819bd27ce
fly2moon2 <fly2moon2@yahoo.com>
2023-05-27 20:24:53 +0800

    json2epp struggling

--- game/src/core/envfa.rs
+++ game/src/core/envfa.rs
@@ -1,5 +1,6 @@
 use serde_json::{Number, Value};
 use serde_derive::{Deserialize, Serialize};
+use serde_json::Map;
 
 use crate::core::env::{Prop};
 
@@ -86,10 +87,30 @@ pub fn test_json_dyn_read() {
 }
 
 
+pub fn json_to_env_prop_pack(json_obj: &Map<String, Value>) -> String {
+    let mut o_result="".to_string();
+/*     if json_obj.is_string() {
+        let mut o_result = json_obj;
+    } else { */
+        for (key, value) in json_obj.iter() {
+/*             println!("j2e println {}: {}", key.clone(), match *value {
+                //Value::U64(v) => format!("{} (u64)", v),
+                Value::String(ref v) => format!("{} (string)", v),
+                _ => format!("other")
+            }); */
+            let mut o_result = match *value {
+                Value::String(ref v) => v.to_string(),
+                Value::Object(ref map)=> json_to_env_prop_pack(map),
+                _ => "".to_string(),
+            };
+        }
+    // }
 
+    o_result
+}
 
 pub fn test_read_json_prop_file() {  
-    let input_path = "./assets/app_properties copy.json";
+    let input_path = "./assets/app_properties.json";
 
     let mut app_props = {
         // Load the first file into a string.
@@ -99,12 +120,21 @@ pub fn test_read_json_prop_file() {
         serde_json::from_str::<Value>(&app_props).unwrap()
     };
 
-    println!("app_properties.json: {}",app_props["env_props"]["locale"].to_string());
+    //println!("app_properties.json: {}",app_props["env_props"]["locale"].to_string());
     // Get the number of items in the object 'env_props'
     let item_cnt = app_props["env_props"]["properties"].as_array().unwrap().len();
     //let item_cnt = app_props["env_props"]["properties"][0].as_object().unwrap().len();
 
     for index in 0..item_cnt{
+        let mut obj0 = app_props["env_props"]["properties"][index].as_object().unwrap();
+        for (key0, value0) in obj0.iter() {
+            let mut a_map=Map::new();
+            //let a_map0=value0.clone();
+            //a_map.insert(key0.to_string(), a_map0);
+            a_map.insert(key0.to_string(), value0.clone());
+            println!("j2e: {}", json_to_env_prop_pack(&a_map));
+        }
+
         let mut obj = app_props["env_props"]["properties"][index].as_object().unwrap();
 
         for (key, value) in obj.iter() {
@@ -117,11 +147,14 @@ pub fn test_read_json_prop_file() {
             //let mut obj1 = app_props["env_props"]["properties"][index][key.to_string()].as_object().unwrap();
             let mut obj1 = app_props["env_props"]["properties"][index][key][0].as_object().unwrap();
             for (key1, value1) in obj1.iter() {
-                let o_value1 = match *value1 {
+/*                 let o_value1 = match *value1 {
                     //Value::U64(v) => format!("{} (u64)", v),
-                    Value::String(ref v) => format!("{} (string)", v),
-                    _ => format!("other")
+                    Value::String(ref v) => v,
+                    Value::Object(ref v) => v,
                 };
+                for  (k, v) in o_value1.iter() {
+                    println!("key={}, value={}", k, v);
+                  } */
                 println!("{}: {}", key1.clone(), match *value1 {
                     //Value::U64(v) => format!("{} (u64)", v),
                     Value::String(ref v) => format!("{} (string)", v),
