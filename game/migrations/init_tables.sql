2d392290db109d98b73eda1cd4bcd4d253680c9b
fly2moon2 <fly2moon2@yahoo.com>
2023-06-18 21:07:08 +0800

    fechain uright

--- game/migrations/init_tables.sql
+++ game/migrations/init_tables.sql
@@ -1,33 +1,84 @@
 -- Add migration script here
+-- user
 CREATE TABLE IF NOT EXISTS uuser (
    id bigserial PRIMARY KEY,
    name VARCHAR (80) UNIQUE NOT NULL,
    display_name VARCHAR (80),
+   typ VARCHAR(10) NOT NULL,
    isactive BOOLEAN DEFAULT TRUE
 ); 
 
 CREATE TABLE IF NOT EXISTS ulogin (
    id bigserial PRIMARY KEY,
    uuser_id bigint NOT NULL,
+   --rel_uuser bigint REFERENCES uuser,
    login VARCHAR (50) UNIQUE NOT NULL,
+   typ VARCHAR(10) NOT NULL,
    start_dte TIMESTAMP DEFAULT NOW(),
    end_dte TIMESTAMP,
-   frez_start_dte TIMESTAMP,
-   frez_end_dte TIMESTAMP,
+   friz_start_dte TIMESTAMP,
+   friz_end_dte TIMESTAMP,
    constraint fk_uuser foreign key (uuser_id) references uuser(id) on delete cascade
 ); 
 
 CREATE TABLE IF NOT EXISTS upasswd (
    id bigserial PRIMARY KEY,
    ulogin_id bigint NOT NULL,
-   passwd VARCHAR (255) NOT NULL,
+   pass VARCHAR (255) NOT NULL,
    start_dte TIMESTAMP DEFAULT NOW(),
    end_dte TIMESTAMP,
    constraint fk_ulogin foreign key (ulogin_id) references ulogin(id) on delete cascade
 ); 
 
+-- user role
+CREATE TABLE IF NOT EXISTS urole (
+   id bigserial PRIMARY KEY,
+   name VARCHAR (80) UNIQUE NOT NULL,
+   isactive BOOLEAN DEFAULT TRUE,
+   parent_urole_id bigint NOT NULL
+); 
+
+CREATE TABLE IF NOT EXISTS uuser_urole (
+   id bigserial PRIMARY KEY,
+   uuser_id bigint NOT NULL REFERENCES uuser,
+   urole_id bigint NOT NULL REFERENCES urole,
+   constraint unique_uuser_and_urole unique (uuser_id, urole_id)
+); 
+
+-- resource
+CREATE TABLE IF NOT EXISTS resauce (
+   id bigserial PRIMARY KEY,
+   name VARCHAR (80) UNIQUE NOT NULL,
+   isactive BOOLEAN DEFAULT TRUE
+); 
+
+-- user role rights
+CREATE TABLE IF NOT EXISTS uright (
+   id bigserial PRIMARY KEY,
+   resauce_id bigint NOT NULL REFERENCES resauce,
+   urole_id bigint NOT NULL REFERENCES urole,
+   right_typ VARCHAR(50) NOT NULL,
+   start_dte TIMESTAMP DEFAULT NOW(),
+   end_dte TIMESTAMP,
+   friz_start_dte TIMESTAMP,
+   friz_end_dte TIMESTAMP,
+   constraint fk_resauce foreign key (resauce_id) references resauce(id) on delete cascade
+); 
+
 -- drop
-DROP TABLE uuser;
-DROP TABLE ulogin;
+DROP TABLE uright;
+DROP TABLE resauce;
+
+DROP TABLE uuser_urole;
+DROP TABLE urole;
+
 DROP TABLE upasswd;
+DROP TABLE ulogin;
+DROP TABLE uuser;
+
+
+
+
+
+
 
--- game/src/types/fechain.rs
+++ game/src/types/fechain.rs
@@ -3,7 +3,7 @@ use std::error::Error;
 use sqlx::types::chrono::{DateTime, Utc};
 
 
-
+/// users
 #[derive(Deserialize, Serialize, Debug, Clone)]
 pub struct Uuser {
    //#[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
@@ -15,25 +15,48 @@ pub struct Uuser {
 }
 
 /// new type idiom
-#[derive(Deserialize, Serialize, Debug, Clone, PartialEq, Eq, Hash)]
+#[derive(Deserialize, Serialize, Debug, Clone)]
 pub struct UuserId(pub i64);
 
+/// user types
+/// standard - ordinary/normal users
+/// admin - administrators/support
+/// system - system-virtual user account
+#[derive(Deserialize, Serialize, Debug, Clone)]
+pub enum UuserTyp {
+    Standard,
+    Admin,
+    System,
+}
+
+/// user login/token
+/// start/end date: valid period
+/// friz_start/end_dte: any freeze/frozen (e.g. account locked within the valid period) 
 #[derive(Deserialize, Serialize, Debug, Clone)]
 pub struct Ulogin {
    //#[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
    pub id: Option<i64>,
    pub login: UloginLogin,
+   pub typ: UloginTyp,
    pub start_dte: DateTime<Utc>,
    pub end_dte: DateTime<Utc>,
-   pub frez_start_dte: DateTime<Utc>,
-   pub frez_end_dte: DateTime<Utc>,
+   pub friz_start_dte: DateTime<Utc>,
+   pub friz_end_dte: DateTime<Utc>,
    pub uuser_id: i64,
    pub upasswd: Option<Vec<Upasswd>>,
 }
 
-#[derive(Deserialize, Serialize, Debug, Clone, PartialEq, Eq, Hash)]
+#[derive(Deserialize, Serialize, Debug, Clone)]
 pub struct UloginLogin(pub String);
 
+#[derive(Deserialize, Serialize, Debug, Clone)]
+pub enum UloginTyp {
+    Login,
+    LoginNToken,
+    Token,
+}
+
+/// user password and validity
 #[derive(Deserialize, Serialize, Debug, Clone)]
 pub struct Upasswd {
    //#[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
@@ -44,5 +67,61 @@ pub struct Upasswd {
    pub ulogin_id: i64,
 }
 
-#[derive(Deserialize, Serialize, Debug, Clone, PartialEq, Eq, Hash)]
+#[derive(Deserialize, Serialize, Debug, Clone)]
 pub struct UpasswdPass(pub String);
+
+/// user role
+/// parent_urole_id - points to the parent role (null for root level)
+#[derive(Deserialize, Serialize, Debug, Clone)]
+pub struct URole {
+   //#[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
+   pub id: Option<i64>,
+   pub name: String,
+   pub isactive: bool,
+   pub parent_urole_id: Option<i64>,
+}
+
+/// mapping of user & roles
+#[derive(Deserialize, Serialize, Debug, Clone)]
+pub struct UuserUrole {
+   //#[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
+   pub id: Option<i64>,
+   pub uuser_id: i64,
+   pub urole_id: i64,
+}
+
+/// application resource
+#[derive(Deserialize, Serialize, Debug, Clone)]
+pub struct Resauce {
+   //#[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
+   pub id: Option<i64>,
+   pub name: String,
+   pub isactive: bool,
+}
+
+/// user role rights
+/// start/end date: valid period
+/// friz_start/end_dte: any freeze/frozen (e.g. account locked within the valid period) 
+#[derive(Deserialize, Serialize, Debug, Clone)]
+pub struct Uright {
+   //#[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
+   pub id: Option<i64>,
+   pub resauce_id: i64,
+   pub urole_id: i64,
+   pub right_typ: UrightTyp,
+   pub start_dte: DateTime<Utc>,
+   pub end_dte: DateTime<Utc>,
+   pub friz_start_dte: DateTime<Utc>,
+   pub friz_end_dte: DateTime<Utc>,
+}
+
+/// types of user role rights
+/// ReadOrAccess - read only, open, general execution access
+/// ReadWrite - read and write
+/// other function-specific rights, with custom string as label
+#[derive(Deserialize, Serialize, Debug, Clone)]
+pub enum UrightTyp {
+    ReadOrAccess,
+    ReadWrite,
+    SpecificRight(String),
+}
